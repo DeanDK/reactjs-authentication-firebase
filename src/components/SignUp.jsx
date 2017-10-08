7ac848f8b25adf02c3a1ca19d5a5e9bb2d306272
DeanDK <deanbozic12@gmail.com>
2017-10-08 14:24:02 +0100

    Added redux for authentication

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 import {firebaseApp} from '../firebase';
 
-class App extends Component {
+class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
-      error:{
+      error: {
         message: ''
       }
     }
@@ -22,18 +23,27 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="form-inline" style={{margin: '5%'}}>
+      <div className="form-inline" style={{
+        margin: '5%'
+      }}>
         <h2>Sign Up</h2>
         <div className="form-group">
-          <input className="form-control" type="text" style={{marginRight: '5%'}} placeholder="email" onChange={event => this.setState({email: event.target.value})}/>
-          <input className="form-control" type="password" style={{marginRight: '5%'}} placeholder="password" onChange={event => this.setState({password: event.target.value})}/>
+          <input className="form-control" type="text" style={{
+            marginRight: '5%'
+          }} placeholder="email" onChange={event => this.setState({email: event.target.value})}/>
+          <input className="form-control" type="password" style={{
+            marginRight: '5%'
+          }} placeholder="password" onChange={event => this.setState({password: event.target.value})}/>
           <button className="btn btn-primary" type="button" onClick={() => this.signUp()}>Sign Up
           </button>
         </div>
         <div>{this.state.error.message}</div>
+        <div>
+          <Link to={'/signin'}>Sign In instead</Link>
+        </div>
       </div>
     )
   }
 }
 
-export default App;
+export default SignUp;
